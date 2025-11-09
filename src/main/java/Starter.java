6fe66227da0ecbb069dc4d26a2acb6e7586ee285
Inuka-bro <inukaasith30@gmail.com>
2025-11-09 22:59:17 +0530

    Implemented UI in LoginForm.fxml

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -5,15 +5,28 @@ import javafx.stage.Stage;
 
 public class Starter extends Application {
 
+    public Stage loginFormstage;
+    private Stage stage;
+
     public static void main(String[] args){
         launch();
     }
 
+    public void closeLoginForm(){
+        System.out.println(this.loginFormstage);
+        //loginFormstage.close();
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-//        Parent root= ;
-//        Scene scene=new Scene(root);
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"))));
+
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
         stage.show();
+        loginFormstage=stage;
+
+        System.out.println(loginFormstage);
     }
+
+
 }
