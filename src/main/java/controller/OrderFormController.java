b9c5343aebfd9b60f1e28aa7acf499259e926b37
Inuka-bro <inukaasith30@gmail.com>
2025-11-16 07:57:58 +0530

    Implement AddtoCart Function for OrderForm

--- src/main/java/controller/OrderFormController.java
+++ src/main/java/controller/OrderFormController.java
@@ -1,14 +1,23 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTreeTableView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.AddToCard;
 
-public class OrderFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrderFormController implements Initializable {
 
     @FXML
     private JFXButton btnAddtoCard;
@@ -23,7 +32,28 @@ public class OrderFormController {
     private JFXButton btnPlaceOrder1;
 
     @FXML
-    private ComboBox<?> cmbProductID;
+    private ComboBox<String> cmbCategory;
+
+    @FXML
+    private ComboBox<String> cmbProductID;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colProductId;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
 
     @FXML
     private Label lblNetTotal;
@@ -35,10 +65,7 @@ public class OrderFormController {
     private Label lblOrderID;
 
     @FXML
-    private JFXTreeTableView<?> tblAddToCard;
-
-    @FXML
-    private TextField txtCategory;
+    private TableView<AddToCard> tblAddToCard;
 
     @FXML
     private TextField txtDiscount;
@@ -52,9 +79,24 @@ public class OrderFormController {
     @FXML
     private TextField txtQty;
 
+    ObservableList<AddToCard> addToCardObservableList= FXCollections.observableArrayList();
+
     @FXML
     void btnAddtoCardOnAction(ActionEvent event) {
+        String productIDValue = cmbProductID.getValue();
+        String nameText = txtProductName.getText();
+        Integer qtyText = Integer.parseInt(txtQty.getText());
+        Double priceText = Double.parseDouble(txtPrice.getText());
+        String categoryValue = cmbCategory.getValue();
+        Integer discountText = Integer.parseInt(txtDiscount.getText());
 
+        addToCardObservableList.add(new AddToCard(productIDValue,nameText,categoryValue,priceText,qtyText,discountText));
+
+        loadAddtoCardTable();
+    }
+
+    private void loadAddtoCardTable() {
+        tblAddToCard.setItems(addToCardObservableList);
     }
 
     @FXML
@@ -63,8 +105,24 @@ public class OrderFormController {
     }
 
     @FXML
+    void cmbCategoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
     void cmbProductIDOnAction(ActionEvent event) {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colProductId.setCellValueFactory(new PropertyValueFactory<>("productId"));
+        colCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        colDiscount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+
+        lblOrderDate.setText(GetDate.getDate());
+    }
 }
